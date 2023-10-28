81878c3bb0d4fff7b3773805b81b4a942b42116d
HSN <hasanlock@gmail.com>
2023-10-28 17:13:21 +0600

    feat: finalize csv writer

--- sed_parser/writer.py
+++ sed_parser/writer.py
@@ -7,11 +7,12 @@ Writer module
 
 
 import abc
+import csv
 import sys
 
 import typing_extensions as tx
 
-from sed_parser.extractor.base import Content
+from sed_parser.extractor.base import Content, FormatList
 
 
 class StreamWriter:
@@ -32,17 +33,21 @@ class StreamWriter:
         print(self.content, file=self.stream)
 
 
-class JsonStreamWriter(StreamWriter):
-    """JsonStreamWriter"""
-
-    @tx.override
-    def format(self, content: Content) -> None:
-        return super().format(content)
-
-
 class CsvStreamWriter(StreamWriter):
     """CsvStreamWriter"""
 
     @tx.override
     def format(self, content: Content) -> None:
-        return super().format(content)
+        csv_writer = csv.writer(self.stream)
+
+        csv_writer.writerow(content.header)
+        csv_writer.writerows(content.body)
+
+
+def select_stream_writer(s_type: str) -> StreamWriter:
+    """select_stream_writer"""
+
+    if s_type == FormatList.CSV.value:
+        return CsvStreamWriter()
+
+    raise SystemExit("error: unsupported writer.")
