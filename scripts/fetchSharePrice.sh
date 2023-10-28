b612334983a5d5823715d8ede8518ff7dacd4a84
HSN <hasanlock@gmail.com>
2023-10-28 17:22:02 +0600

    feat: update script to dump from curl

--- scripts/fetchSharePrice.sh
+++ scripts/fetchSharePrice.sh
@@ -1,5 +1,3 @@
 #! /usr/bin/env bash
 
-cd storage/ || exit
-curl "https://www.dsebd.org/latest_share_price_scroll_l.php" -s -o "latest_share_price_scroll_l-$(date +"%Y%m%d").html"
-cd - || exit
+curl https://www.dsebd.org/latest_share_price_scroll_l.php -s | python -m sed_parser --site dse --page-type sharePrice --format csv > "dumps/dse/share_price/$(date +"%Y-%m-%d").csv"
