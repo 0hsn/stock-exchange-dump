64738690ff38b034f22177e94048b392dbbc77ef
HSN <hasanlock@gmail.com>
2023-10-28 16:23:05 +0600

    refactor: remove writers

--- sed_parser/extractor/base.py
+++ sed_parser/extractor/base.py
@@ -4,10 +4,9 @@ Extractor module
 
 from __future__ import annotations
 from abc import ABC, abstractmethod
-from datetime import datetime
 import dataclasses
+import enum
 
-from sed_parser.writer import FileWriter
 
 class SiteList(enum.Enum):
     """SiteList"""
@@ -65,26 +64,3 @@ class Extractor(ABC):
     @abstractmethod
     def extract(self):
         """extract signature"""
-
-
-class ExtractWriter:
-    """Extract Writer"""
-
-    def __init__(self, filename_parts: list[str]) -> None:
-        self.filename_parts = filename_parts
-
-    @property
-    def file_to_write(self):
-        """file_to_write"""
-
-        filename_parts = [part.lower for part in self.filename_parts]
-
-        now = datetime.now()
-        filename_parts.append(now.strftime("%Y-%m-%d"))
-
-        return "-".join(filename_parts)
-
-    def write(self, writer: FileWriter):
-        """write to file"""
-
-        writer.write_file(self.file_to_write)
