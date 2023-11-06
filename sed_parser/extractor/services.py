24815f48ff8144678cb0fa6a959c51882b825dc7
HSN <hasanlock@gmail.com>
2023-11-06 20:49:13 +0600

    feat: write to file when file found

--- sed_parser/extractor/services.py
+++ sed_parser/extractor/services.py
@@ -1,16 +1,32 @@
 """
 Extractor service
 """
-from sed_parser.extractor.base import CommandArgs, SiteList, PageTypeList
+from configparser import ConfigParser
+
+from sed_parser.extractor.base import (
+    CommandArgs,
+    CommandSettings,
+    SiteList,
+    PageTypeList,
+)
 from sed_parser.extractor.dse import SharePriceExtractor
 from sed_parser.writer import select_stream_writer
+
+
+def get_config_parser(cfg_path: str) -> ConfigParser:
+    """get_filepath_for_content"""
+
+    cfg_parser = ConfigParser()
+    cfg_parser.read(cfg_path)
+
+    return cfg_parser
 
 
 class ExtractionOp:
     """Extract Writer"""
 
     @staticmethod
-    def from_arg(args: CommandArgs) -> None:
+    def from_arg(args: CommandArgs, settings: CommandSettings) -> None:
         """parse dse share price"""
 
         extractor = None
@@ -24,9 +40,12 @@ class ExtractionOp:
         if extractor:
             content = extractor.extract()
 
-            writer = select_stream_writer(args.format)
+            _s_stream_path = settings.share_price_path_fmt.format(
+                filename=content.date_on_page.strftime("%Y-%m-%d")
+            )
+
+            writer = select_stream_writer(args.format, **dict(file=_s_stream_path))
             writer.format(content)
-            writer.dump()
 
             return
 
