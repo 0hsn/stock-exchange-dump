1b1f8585169ed5ba2fa6a6eb92e738cde8091cd9
HSN <hasanlock@gmail.com>
2023-11-17 12:00:54 +0600

    refactor: extraction operation

--- sed_parser/services.py
+++ sed_parser/services.py
@@ -6,12 +6,12 @@ from configparser import ConfigParser
 from sed_parser.extractor.base import (
     CommandArgs,
     CommandSettings,
-    DataFrameBuilder,
     PricePageTableContent,
     SiteList,
     PageTypeList,
 )
 from sed_parser.extractor.dse import SharePriceExtractor
+from sed_parser.transformers import DataFrameBuilder
 from sed_parser.writer import select_stream_writer
 
 
@@ -27,33 +27,35 @@ def get_config_parser(cfg_path: str) -> ConfigParser:
 class ExtractionOp:
     """Extract Writer"""
 
-    @staticmethod
-    def from_arg(args: CommandArgs, settings: CommandSettings) -> None:
+    @classmethod
+    def from_arg(cls, args: CommandArgs, settings: CommandSettings) -> None:
         """parse dse share price"""
-
-        extractor = None
 
         if (
             args.site == SiteList.DSE.value
             and args.page_type == PageTypeList.SHARE_PRICE.value
         ):
-            extractor = SharePriceExtractor(args.data)
+            cls._process_share_price_extractor_(args, settings)
+        else:
+            raise SystemExit("error: site or page-type mismatch.")
 
-        if extractor:
-            content = extractor.extract()
+    @classmethod
+    def _process_share_price_extractor_(
+        cls, args: CommandArgs, settings: CommandSettings
+    ) -> None:
+        """_process_share_price_extractor_"""
 
-            df = DataFrameBuilder.from_price_page_table_content(content)
+        extractor = SharePriceExtractor(args.data)
+        content = extractor.extract()
 
-            content_upd = PricePageTableContent.from_data_frame(df)
-            content_upd.page_updated_date = content.page_updated_date
+        df = DataFrameBuilder.from_price_page_table_content(content)
 
-            _s_stream_path = settings.share_price_path_fmt.format(
-                filename=content.date_on_page.strftime("%Y-%m-%d")
-            )
+        content_upd = PricePageTableContent.from_data_frame(df)
+        content_upd.page_updated_date = content.page_updated_date
 
-            writer = select_stream_writer(args.format, **dict(file=_s_stream_path))
-            writer.format(content_upd)
+        _s_stream_path = settings.share_price_path_fmt.format(
+            filename=content.date_on_page.strftime("%Y-%m-%d")
+        )
 
-            return
-
-        raise SystemExit("error: site or page-type mismatch.")
+        writer = select_stream_writer(args.format, **dict(file=_s_stream_path))
+        writer.format(content_upd)
