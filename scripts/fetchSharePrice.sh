8c2720bbf590eb9c34ba73f6692e776216d1e05f
HSN <3197377+0hsn@users.noreply.github.com>
2024-01-06 18:02:48 +0600

    ci: init 2

--- scripts/fetchSharePrice.sh
+++ scripts/fetchSharePrice.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-curl https://www.dsebd.org/latest_share_price_scroll_l.php -s | python -m sed_parser --site dse --page-type sharePrice --format csv > "dumps/dse/share_price/$(date +"%Y-%m-%d").csv"
+curl https://www.dsebd.org/latest_share_price_scroll_l.php -s | python -m sed_parser --site dse --page-type sharePrice --format csv
